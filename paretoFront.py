a2c8cbfb6cb05b1141465560517721e514926e6d
oliwi <oliwiakowalik2@gmail.com>
2022-03-29 14:46:59 +0200

    front pareto, now it works

--- paretoFront.py
+++ paretoFront.py
@@ -3,14 +3,22 @@ import math
 import matplotlib.pyplot as plt
 from scipy.optimize import minimize
 
-iterations = 500
+iterations = 2000
+
 
 def f1(x):
-    return x
+    if x < -math.pi or x > math.pi:
+        return abs(x)*1000000
+    else:
+        return x
 
 
 def f2(x):
-    return 1-(1/4*math.pi**2)*(x[0]+math.pi)**2+abs(x[1]-5*math.cos(x[0]))**(1/3)+abs(x[2]-5*math.sin(x[0]))**(1/3)
+    temp = 1-(1/(4*(math.pi**2)))*((x[0]+math.pi)**2)+(abs(x[1]-5*math.cos(x[0])))**(1/3)+(abs(x[2]-5*math.sin(x[0])))**(1/3)
+    if x[0] < -math.pi or x[0] > math.pi or x[1] < -5 or x[1] > 5 or x[2] < -5 or x[2] < -5:
+        return abs(temp) * 1000000
+    else:
+        return temp
 
 
 def f(x):
@@ -21,6 +29,8 @@ def f(x):
 x = np.ndarray(shape=(iterations, 3))
 w = np.ndarray(shape=(iterations, 2))
 y = np.ndarray(shape=(iterations, 2))
+y2 = np.ndarray(shape=(iterations, 2))
+
 
 for i in range(iterations):
     x1 = np.random.uniform(-math.pi, math.pi)
@@ -31,11 +41,12 @@ for i in range(iterations):
     w2 = 1 - w1
     w[i, :] = w1, w2
 
-    args = [x1, x2, x3]
+    args = np.ndarray((3, 1), dtype=np.float64)
+    args = x1, x2, x3
     minimized = minimize(f, args, method='nelder-mead')
     x[i, :] = minimized['x']
-    y[i, :] = [f1(x1), f2([x1, x2, x3])]
+    y[i, :] = [f1(x[i, 0]), f2(x[i, :])]
+    
 
 plt.scatter(y[:, 0], y[:, 1])
-plt.ylim(-100, 10)
 plt.show()
